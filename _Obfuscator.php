3ac1064798c9cff791e4e1374787095c8b574250
BlueEyesHF <socialthefoxdie@gmail.com>
2016-10-09 01:11:34 +0200

    Add files via upload

--- _Obfuscator.php
+++ _Obfuscator.php
@@ -11,11 +11,10 @@ function Encode($output) {
     $randomOut = randomString();
     $randomNum = randomString();
     $randomVal = mt_rand(999999, 99999999);
-    $return = '<script>String.prototype.decode = function(){ return decodeURIComponent(escape(this)); } ' . PHP_EOL;
-    $return .= 'var ' . $randomOut . ' = ""; var ' . $randomNum . ' = [';
+    $return = '<script>var ' . $randomOut . ' = ""; var ' . $randomNum . ' = [';
     foreach(str_split($output) as $x) $return .= '"'.base64_encode(randomString().(ord($x) + $randomVal)) . '", ';
     $return = rtrim($return, ', ');
-    $return .= ']; ' . $randomNum . '.forEach(function ' . $randomFunc . '(value) { ' . $randomOut . ' += String.fromCharCode(parseInt(atob(value).replace(/\D/g,\'\')) - ' . $randomVal . '); } ); document.write(' . $randomOut . '.decode()); </script>'  ;;
+    $return .= ']; ' . $randomNum . '.forEach(function ' . $randomFunc . '(value) { ' . $randomOut . ' += String.fromCharCode(parseInt(atob(value).replace(/\D/g,\'\')) - ' . $randomVal . '); } ); document.write(decodeURIComponent(escape(' . $randomOut . '))); </script>'  ;;
     return $return;
 }
 
