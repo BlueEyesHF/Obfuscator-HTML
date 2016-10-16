4d58b4500b04545572288afd07dee88e36003b06
BlueEyesHF <socialthefoxdie@gmail.com>
2016-10-16 11:11:12 +0200

    Add files via upload

--- _Obfuscator.php
+++ _Obfuscator.php
@@ -1,6 +1,5 @@
 <?php
-
-function randomString($length = 6) {
+function randomString($length = 3) {
     $randomString = '';
     $characters = implode("", array_merge(range('a', 'z'), range('A', 'Z')));
     for ($i = 0; $i < $length; $i++) $randomString .= $characters[mt_rand(0, strlen($characters) - 1)];
@@ -11,11 +10,11 @@ function Encode($output) {
     $randomOut = randomString();
     $randomNum = randomString();
     $randomVal = mt_rand(999999, 99999999);
-    $return = '<script>var ' . $randomOut . ' = ""; var ' . $randomNum . ' = [';
-    foreach(str_split($output) as $x) $return .= '"'.base64_encode(randomString().(ord($x) + $randomVal)) . '", ';
+    $return = '<!-- Obfuscator HTML : https://github.com/BlueEyesHF/Obfuscator-HTML -->
+<script>var ' . $randomOut . ' = ""; var ' . $randomNum . ' = [';
+    foreach(str_split($output) as $x){ $return .= '"'.base64_encode(randomString().(ord($x) + $randomVal).randomString()) . '", '; if (mt_rand(0, 1)){ $return .= "\n"; } }
     $return = rtrim($return, ', ');
     $return .= ']; ' . $randomNum . '.forEach(function ' . $randomFunc . '(value) { ' . $randomOut . ' += String.fromCharCode(parseInt(atob(value).replace(/\D/g,\'\')) - ' . $randomVal . '); } ); document.write(decodeURIComponent(escape(' . $randomOut . '))); </script>'  ;;
     return $return;
 }
-
 ob_start("Encode");
